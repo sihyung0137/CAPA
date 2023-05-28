6bd36ac680b833a5e955275d994b06490029ef60
sihyung0137 <sihyung6645@gmail.com>
2023-05-28 13:34:29 +0900

    Merge branch 'main' of https://github.com/sihyung0137/CAPA.git into main
    survey + review commit 05 28 sihyung

---
+++ capa/src/main/java/net/softsociety/spring03/service/SurveyService.java
@@ -0,0 +1,17 @@
+package net.softsociety.spring03.service;
+
+import java.util.ArrayList;
+
+import net.softsociety.spring03.vo.Survey;
+
+public interface SurveyService {
+
+	ArrayList<Survey> surveyList();
+
+	Survey read(int surveynum);
+
+	int write(Survey survey);
+
+	int delete(Survey survey);
+
+}
